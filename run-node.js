9b93343915be41a3a0d4e6e5da9e41f2985c19e2
Giancarlo Anemone <ganemone@uber.com>
2016-04-29 18:02:02 -0700

    Fix path resolution for node environment tests

--- run-node.js
+++ run-node.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var path = require('path');
 var spawn = require('child_process').spawn;
 var finished = require('tap-finished');
+var cwd = process.cwd();
 
 function runNode(entry, cb) {
-  var script = 'require("./ipc-helpers/dump-node.js");require("' + entry + '");';
+  var dumpNodePath = path.join(__dirname, './ipc-helpers/dump-node.js');
+  var absoluteEntryPath = path.resolve(cwd, entry);
+  var script = 'require("' + dumpNodePath + '");require("' + absoluteEntryPath + '");';
   var child = spawn('node', [], {stdio: [null, null, null, 'ipc']});
   child.on('message', function(message) {
     cb(message.coverage);
