7bf4ebfc44a67373c6e54f4714a750139c40a253
Aleksey Smolenchuk <aleksey@uber.com>
2016-05-11 20:16:00 -0700

    add istanbul-reporter-raw-json to report raw json

--- report-coverage.js
+++ report-coverage.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var istanbul = require('istanbul');
+var rawJsonReporter = require('istanbul-reporter-raw-json');
+istanbul.Report.register(rawJsonReporter);
 
 function report(objects, reports) {
   var collector = new istanbul.Collector();
